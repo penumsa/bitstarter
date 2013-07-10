ec45fb72187fd32bbdbf164f9582e79e5374945a
Sastry Penumarthy <sastry.penumarthy@gmail.com>
2013-07-10 06:26:38 +0000

    Updated web.js with read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var buf = new Buffer(fs.readFileSync("index.html"));
+var outie = buf.toString("utf-8");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(outie);
 });
 
 var port = process.env.PORT || 5000;
