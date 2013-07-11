0faded15e96478f3eb40b9269b1abeb62c5f54f7
Sastry Penumarthy <sastry.penumarthy@gmail.com>
2013-07-11 09:07:28 +0000

    Added grader.js

--- grader.js
+++ grader.js
@@ -1,31 +1,12 @@
-#!/usr/bin/env node
-/*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
-
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://floating-citadel-8367.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +42,39 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var myURLChecker = function(url_entered) {
+   rest.get(url_entered).on('complete', function(result) {
+        if (result instanceof Error) { 
+           sys.puts('Error: ' + result.message); this.retry(5000); // try again after 5 sec
+        } else { 
+                fs.writeFile("result.json", result, function(err) { 
+                            if(err) { console.log(err); 
+                            } else {
+                                     var checkJson_html = checkHtmlFile("result.json", program.checks);
+                                     var outJson = JSON.stringify(checkJson_html, null, 4);
+                                     console.log(outJson);
+                                   };
+                 });
+        };
+    });
+};
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url', 'Path to url', clone(myURLChecker), URL_DEFAULT)
         .parse(process.argv);
+
+    if (program.file) {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    } else {
+           if (program.url) {
+              var checkJson = myURLChecker(process.argv[5], program.checks);
+           };
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
-}
+} 
